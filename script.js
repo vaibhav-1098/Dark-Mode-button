2f01174294b62e4fa2c5a7e59bea0f858338ee15
vaibhav-1098 <vaibhav.2003.rana@gmail.com>
2024-05-15 11:13:18 +0530

    _

---
+++ script.js
@@ -0,0 +1,38 @@
+// slide animation 
+const slider=document.querySelector(".slider");
+
+slider.addEventListener('click',( )=>{
+    document.body.classList.toggle("move");
+})
+
+
+// dark mode  
+slider.addEventListener('click',( )=>{
+    document.body.classList.toggle("dark");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
